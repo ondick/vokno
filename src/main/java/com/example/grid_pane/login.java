2d50d02edc82f6a967bf12e96df02d641cde583d
musond12 <musond12@gasos-ro.cz>
2025-11-20 10:50:14 +0100

    kkk

--- src/main/java/com/example/grid_pane/login.java
+++ src/main/java/com/example/grid_pane/login.java
@@ -7,6 +7,7 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 
 public class login extends Application {
@@ -14,6 +15,8 @@ public class login extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
+
+        scene.getStylesheets().add(getClass().getResource("Styl.css").toExternalForm());
         stage.setTitle("Registrace uzivatele");
         stage.setScene(scene);
         stage.show();
